19f06c40fc687004260a2eac0078c37c8e2bffda
Git-Prateek <prateekthorat141@gmail.com>
2023-05-08 21:29:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+
+let timeoutRef = null;
+export default function App() {
+  const [count, setCount] = React.useState(0);
+  const [isStarted, setIsStated] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isStarted) {
+      updateCounter(count + 1);
+    }
+  }, [count]);
+
+  const updateCounter = (countTep) => {
+    timeoutRef = setTimeout(() => {
+      setCount(countTep);
+    }, 1000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h2>Counter {count} </h2>
+
+      <button
+      class="glow-on-hover" 
+        onClick={() => {
+          if (clearTimeout) clearTimeout(timeoutRef);
+          setIsStated(!isStarted);
+          if (!isStarted) setCount(count + 1);
+        }}
+      >
+        {!isStarted ? "Start" : "Pause"}
+      </button>
+      <button
+      class="glow-on-hover" 
+        onClick={() => {
+          if (clearTimeout) clearTimeout(timeoutRef);
+          setIsStated(false);
+          setCount(0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
-
-export default App;
